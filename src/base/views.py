9c57992c8a304964810e9735b10e276745bb11e9
godkvng <stryder7x@protonmail.com>
2023-07-13 16:41:47 +0100

    Rebuilding the create-room form without using `Django Form Model`

--- src/base/views.py
+++ src/base/views.py
@@ -127,15 +127,21 @@ def create_room(request):
 	form = RoomForm()
 	topics = Topic.objects.all()
 	if request.method == 'POST':
-		form = RoomForm(request.POST)
+		# form = RoomForm(request.POST)
 		topic_name = request.POST.get('topic')
 		topic, created = Topic.objects.get_or_create(name=topic_name)
-		if form.is_valid():
-			room = form.save(commit=False)
-			# Host will be added based on logged in user
-			room.host = request.user
-			room.save()
-			return redirect('home')
+		Room.objects.create(
+			host=request.user,
+			topic=topic,
+			name=request.POST.get('name'),
+			description=request.POST.get('description'),
+		)
+		# if form.is_valid():
+		# 	room = form.save(commit=False)
+		# 	# Host will be added based on logged in user
+		# 	room.host = request.user
+		# 	room.save()
+		return redirect('home')
 
 	context = {'form': form, 'topics': topics}
 	return render(request, 'base/room_form.html', context)
